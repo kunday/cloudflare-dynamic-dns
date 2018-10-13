36d4d242590566275c530c8115ab75594b2b64cc
Arvind Kunday <akunday@zendesk.com>
2018-10-13 16:42:56 +1100

    A little logging is useful.

--- lib/cloud_flare/dynamic_dns/updater.rb
+++ lib/cloud_flare/dynamic_dns/updater.rb
@@ -17,6 +17,13 @@ module CloudFlare
         connection = Cloudflare.connect(key: key, email: email)
         dns_zone = connection.zones.find_by_name(zone)
         dns_record = dns_zone.dns_records.find_by_name(hostname)
+        puts "=> #{dns_record.record[:name]} currently pointing to -> #{dns_record.record[:content]}"
+        puts "=> current public IP of your network is #{ip}."
+        if ip == dns_record.record[:content]
+          puts '=> No changes detected.'
+          return
+        end
+        puts "=> updating #{dns_record.record[:name]} to #{ip}"
         dns_record.update_content(ip)
       end
 
