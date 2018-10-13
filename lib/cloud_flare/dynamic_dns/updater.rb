eab70d7c1383f2495537cd6fb10a84d951ff3c52
Arvind Kunday <akunday@zendesk.com>
2018-10-13 16:46:26 +1100

    Move methods around.

--- lib/cloud_flare/dynamic_dns/updater.rb
+++ lib/cloud_flare/dynamic_dns/updater.rb
@@ -14,17 +14,30 @@ module CloudFlare
       end
 
       def execute
-        connection = Cloudflare.connect(key: key, email: email)
-        dns_zone = connection.zones.find_by_name(zone)
-        dns_record = dns_zone.dns_records.find_by_name(hostname)
         puts "=> #{dns_record.record[:name]} currently pointing to -> #{dns_record.record[:content]}"
         puts "=> current public IP of your network is #{ip}."
-        if ip == dns_record.record[:content]
+        if changed?
+          puts "=> updating #{dns_record.record[:name]} to #{ip}"
+          dns_record.update_content(ip)
+        else
           puts '=> No changes detected.'
-          return
         end
-        puts "=> updating #{dns_record.record[:name]} to #{ip}"
-        dns_record.update_content(ip)
+      end
+
+      def changed?
+        ip != dns_record.record[:content]
+      end
+
+      def dns_record
+        @dns_record ||= dns_zone.dns_records.find_by_name(hostname)
+      end
+
+      def dns_zone
+        @dns_zone ||= connection.zones.find_by_name(zone)
+      end
+
+      def connection
+        @connection ||= Cloudflare.connect(key: key, email: email)
       end
 
       private
