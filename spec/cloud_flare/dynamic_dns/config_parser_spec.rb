89e76f1c040d3d14127ce02b66c2e0b6d9e7dfde
Arvind Kunday <akunday@zendesk.com>
2018-10-13 15:56:55 +1100

    Fix line length problems.

--- spec/cloud_flare/dynamic_dns/config_parser_spec.rb
+++ spec/cloud_flare/dynamic_dns/config_parser_spec.rb
@@ -2,7 +2,9 @@
 
 RSpec.describe CloudFlare::DynamicDNS::ConfigParser do
   let(:parser) { described_class.new(data: data) }
-  let(:data) { "email=cloudflare-email\napi_key=cloudflare-global-api-key\nzone=cloudflare-domain\nhostname=full-hostname\n" }
+  let(:data) do
+    "email=cloudflare-email\napi_key=cloudflare-global-api-key\nzone=cloudflare-domain\nhostname=full-hostname\n"
+  end
 
   describe '#execute' do
     it 'returns config in hash format' do
